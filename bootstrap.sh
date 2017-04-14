1b88fc6d48ae61d08f23d3d41a2088da3b35e25d
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-04-14 03:04:00 +0200

    Basic in Vagrant install Initial Debian packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,15 @@ echo deb http://v.s.cz/ stable main > /etc/apt/sources.list.d/vitexsoftware.list
 
 export DEBIAN_FRONTEND="noninteractive"
 apt-get update
-apt-get install -y php7.0 php7.0-curl php-pear php7.0-intl
+apt-get install -y php7.0 php7.0-curl php-pear php7.0-intl composer unzip php7.0-zip devscripts
 cd /vagrant
 composer update
-
+cd debian
+./deb-package.sh
+mkdir -p /vagrant/deb
+mv /*.deb /vagrant/deb
+cd /vagrant/deb
+dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+echo "deb file:/vagrant/deb ./" > /etc/apt/sources.list.d/local.list
+apt-get update
+apt-get -y --allow-unauthenticated install flexiproxy flexiproxy-apache
